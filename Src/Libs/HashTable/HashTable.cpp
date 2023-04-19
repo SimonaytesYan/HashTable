d2197ce50f97d6ded02ac05bd7dd5c942ae579a7
Simonaytes <yandexman228@gmail.com>
2023-04-20 00:12:57 +0300

    add dtor for hash table

--- Src/Libs/HashTable/HashTable.cpp
+++ Src/Libs/HashTable/HashTable.cpp
@@ -1,5 +1,24 @@
 #include "HashTable.h"
 
+void HashTableDtor  (HashTable_t* hash_table)
+{
+    for (int i = 0; i < hash_table->table_size; i++)
+    {
+        int index = 0;
+        ListIterate(&hash_table->lists[i], &index);
+        while (index != 0)
+        {
+            Element_t element_i = hash_table->lists[i].data[index].val;
+            free(hash_table->lists[i].data[index].val);
+
+            ListIterate(&hash_table->lists[i], &index);
+        }
+
+        ListDtor(&hash_table->lists[i]);
+    }
+
+    free(hash_table->lists);
+}
 
 void HashTableCtor(HashTable_t* hash_table, size_t table_size, 
                    size_t (*hash_function)(const Element_t object), 
@@ -18,7 +37,6 @@ void HashTableCtor(HashTable_t* hash_table, size_t table_size,
 void HashTableInsert(HashTable_t* hash_table, Element_t element)
 {
     size_t hash = (hash_table->hash_function(element)) % hash_table->table_size;
-
     ListInsert(&(hash_table->lists[hash]), element, 0, nullptr);
 }
 
--- Src/Libs/HashTable/HashTable.h
+++ Src/Libs/HashTable/HashTable.h
@@ -4,7 +4,7 @@
 typedef char* Element_t; 
 
 #include "../List.h"
- 
+
 struct HashTable_t
 {
     size_t  (*hash_function)(Element_t object)         = nullptr;  //<by object returns a hash
@@ -17,7 +17,10 @@ struct HashTable_t
 void HashTableCtor(HashTable_t* hash_table, size_t table_size, 
                    size_t (*hash_function)(const Element_t object), 
                    int    (*comparator)   (const Element_t a, const Element_t b));
+void HashTableDtor  (HashTable_t* hash_table);
 
 void HashTableInsert(HashTable_t* hash_table, Element_t element);
+void HashTableRemove(HashTable_t* hash_table, Element_t element);
+bool HashTableFind  (HashTable_t* hash_table, Element_t element);
 
 #endif
