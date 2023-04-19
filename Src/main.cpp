2bfc87964dc203e94d30eef520fe2ff11498a4ed
Simonaytes <yandexman228@gmail.com>
2023-04-20 00:13:58 +0300

    Put testing hash functions in separeted function

--- Src/main.cpp
+++ Src/main.cpp
@@ -2,6 +2,9 @@
 
 #include "Libs/HashTable/HashTable.h"
 
+const size_t MAX_WORD_LENGTH = 1000;
+const size_t TABLE_SIZE      = 255;
+
 //! this hash function always returns 1
 size_t HashFunction1(const Element_t element)
 {
@@ -28,7 +31,7 @@ size_t HashFunction4(const Element_t element)
     size_t hash = element[i];
     while (element[i] != '\0')
     {
-        hash = ((hash & 1) << 31 + hash >> 1) ^ element[i];
+        hash = (((hash & 1) << 31) + (hash >> 1)) ^ element[i];
         i++;
     }
 
@@ -43,7 +46,7 @@ size_t HashFunction5(const Element_t element)
     size_t hash = element[i];
     while (element[i] != '\0')
     {
-        hash = ((hash & (1 << 31)) >> 31 + hash << 1) ^ element[i];
+        hash = (((hash & (1 << 31)) >> 31) + (hash << 1)) ^ element[i];
         i++;
     }
     
@@ -60,7 +63,7 @@ size_t HashFunction6(const Element_t element)
         hash = hash * 33 + element[i];
         i++;
     }
-    
+
     return hash;
 }
 
@@ -69,21 +72,47 @@ int comparator(Element_t a, Element_t b)
     return strcmp(a, b);
 }
 
+void TestHashFunctions(const char* test_data_file, const char* result_file)
+{
+    FILE* test_data = fopen(test_data_file, "r");
+    FILE* result_fp = fopen(result_file,    "w");
+
+    HashTable_t hash_tables[6] = {};
+
+    HashTableCtor(&hash_tables[0], TABLE_SIZE, HashFunction1, comparator);
+    HashTableCtor(&hash_tables[1], TABLE_SIZE, HashFunction2, comparator);
+    HashTableCtor(&hash_tables[2], TABLE_SIZE, HashFunction3, comparator);
+    HashTableCtor(&hash_tables[3], TABLE_SIZE, HashFunction4, comparator);
+    HashTableCtor(&hash_tables[4], TABLE_SIZE, HashFunction5, comparator);
+    HashTableCtor(&hash_tables[5], TABLE_SIZE, HashFunction6, comparator);
+
+    char word[MAX_WORD_LENGTH] = {0};
+    while (fscanf(test_data, "%s", word) == 1)
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            char* new_word = (char*)calloc(sizeof(char), strlen(word) + 1);
+            memcpy(new_word, word, sizeof(char) * strlen(word));
+            HashTableInsert(&hash_tables[i], new_word);
+        }
+    }
+    fclose(test_data);
+
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < hash_tables[i].table_size; j++)
+            fprintf(result_fp, "%d;", hash_tables[i].lists[j].size);
+        fprintf(result_fp, "\n");
+    }
+    
+
+    for (size_t i = 0; i < 6; i++)
+        HashTableDtor(&hash_tables[i]);
+
+    fclose(result_fp);
+}
+
 int main()
 {
-    HashTable_t hash_table1 = {};
-    HashTable_t hash_table2 = {};
-    HashTable_t hash_table3 = {};
-    HashTable_t hash_table4 = {};
-    HashTable_t hash_table5 = {};
-    HashTable_t hash_table6 = {};
-
-    HashTableCtor(&hash_table1, 100, HashFunction1, comparator);
-    HashTableCtor(&hash_table2, 100, HashFunction2, comparator);
-    HashTableCtor(&hash_table3, 100, HashFunction3, comparator);
-    HashTableCtor(&hash_table4, 100, HashFunction4, comparator);
-    HashTableCtor(&hash_table5, 100, HashFunction5, comparator);
-    HashTableCtor(&hash_table6, 100, HashFunction6, comparator);
-
-    
+    TestHashFunctions("Data/English.txt", "Data/CmpHashFunctions.csv");
 }
